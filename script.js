2eb2197e9b1193122383cb4c6ce879fcf04e521c
neelam <neelamchadokar21@gmail.com>
2020-05-21 22:26:17 +0530

    first commit after completing the interface coding.

---
+++ script.js
@@ -0,0 +1,20 @@
+console.log("java");
+
+let timerObj= {
+	minutes:0,
+	seconds: 0,
+	timerId: 0
+}
+function soundAlarm(){
+	let amount =3;
+	let audio = new Audio("Timer_Sound_Effect.mp3");
+	function playSound(){
+		audio.pause();
+		audio.currentTime = 0;
+		audio.play();
+	}
+
+	for(let i=0; i<amount; i++){
+		setTimeout(playSound, 1200 *i);
+	}
+}
